94928085633cdd8a4927ad9b4092b11dc4c6bbb7
jamescMDS <85146744+JamesCharnley@users.noreply.github.com>
2024-08-04 19:50:59 +1200

    re-style

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ function App() {
     <div style={{width: "100vw"}}>
       <HeaderImage></HeaderImage>
       <div style={{height: "100px"}}></div>
-      <InfoSection imageSide={"right"} imageUrl={"bluemonkeypalms.jpg"} title={"WELCOME"}>
+      <InfoSection imageSide={"right"} imageUrl={"bluemonkeypalms.jpg"} title={"welcome"}>
         Welcome to Lombok Land Ventures, your trusted guide to investing in the stunning island of Lombok, Indonesia.<br/><br/>
          Whether you're a first-time investor or looking to expand your portfolio, our dedicated team is here to help 
          you navigate the process of buying land in this tropical paradise.<br/><br/> With our expertise and local insights, 
          we'll ensure you make informed decisions, unlocking the potential of Lombok's booming real estate market.<br/><br/> 
          Start your journey towards owning a piece of paradise today with Lombok Land Ventures.
         </InfoSection>
-      <div style={{height: "100px"}}></div>
       <InfoSection imageSide={"left"} imageUrl={"meangland.jpg"} title={"WE OFFER"}>
         At Lombok Land Ventures, we specialize in connecting investors directly with local land brokers, allowing you to bypass the big companies that often sell land at inflated prices.<br/><br/>
         Our network of trusted local brokers ensures that you get the best deals on the market, providing you with exclusive opportunities that might otherwise be out of reach.<br/><br/> With our expertise and local insights, 
@@ -27,6 +26,8 @@ function App() {
 
 
       <div style={{width: "40%", marginLeft: "auto", marginRight: "auto"}}>
+        <h3>Get started</h3>
+        <p>fill out the form below and we will send you some options</p>
       <InfoForm />
       </div>
     </div>
